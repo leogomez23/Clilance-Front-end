5dbf8f9ac69a8a2120ab5d56ad0a2968740480e1
Leonardo Gomez <contactoleodevcode@gmail.com>
2020-01-12 07:09:24 -0400

    Primera version funcional

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,21 @@
 import React, {useState, useEffect} from 'react';
-import './assets/styles/From.css'
-import useValidation from '../customHooks/useValidation';
-
+import axios from 'axios';
+import './assets/styles/From.css';
 
 const Form = () => {
-    const [validationSelect, setValidationSelect] = useState(false);
-    const [validationName, setValidationName] = useState(false);
-    const [validationEmail, setValidationEmail] = useState(false);
+    const [validationSelect, setValidationSelect] = useState({
+        state: false,
+        country: null
+    });
+    const [validationName, setValidationName] = useState({
+        state: false,
+        name: null
+    });
+    const [validationEmail, setValidationEmail] = useState({
+        state:false,
+        email: null
+    });
+    const [validationForm, setValidationForm] = useState(false);
 
     function validationStyle (element, validation) {
         if (validation) {
@@ -21,43 +30,81 @@ const Form = () => {
     function handleChangeSelect(e) {
 
         if (e.target.selectedOptions[0].value === 'init') {
-            setValidationSelect(false);
+            setValidationSelect({
+                state: false,
+                country: null
+            });
             validationStyle(e.target, false);
         }
         else {
-            setValidationSelect(true);
+            setValidationSelect({
+                state: true,
+                country: e.target.selectedOptions[0].value
+            });
             validationStyle(e.target, true);
         }
     };
 
     function handleChangeName (e) {
         if (e.target.value.length < 3) {
-            setValidationName(false);
+            setValidationName({
+                state: false,
+                name: null
+            });
             validationStyle(e.target, false);
         }else {
-            setValidationName(true);
+            setValidationName({
+                state: true,
+                name: e.target.value
+            });
             validationStyle(e.target, true);
         }
     }
 
     function handleChangeEmail (e) {
         if (e.target.value.includes("@") && e.target.value.length > 5) {
-            setValidationEmail(true);
+            setValidationEmail({
+                state: true,
+                email: e.target.value
+            });
             validationStyle(e.target, true);
         }else {
-            setValidationEmail(false);
+            setValidationEmail({
+                state: false,
+                email: null
+            });
             validationStyle(e.target, false);
         }
     }
 
-    function handleChangeClick (e) {
-        if (validationSelect && validationName && validationEmail) {
+    function handleChangeSubmit (e) {
+        if (validationSelect.state && validationName.state && validationEmail.state) {
+            setValidationForm(true);
             validationStyle(e.target, true);
         }else {
+            setValidationForm(false);
             validationStyle(e.target, false);
-            alert("Complete todos los campos correctamente para poder enviar")
+            alert("Complete todos los campos correctamente para poder enviar");
         }
     }
+
+    useEffect(() => {
+        if(validationForm) {
+            let suscription = {
+                country: validationSelect.country,
+                name: validationName.name,
+                email: validationEmail.email
+            }
+            console.log(validationEmail)
+            axios.post('/suscription', suscription)
+                .then((respons) => {
+                    alert('Se a enviado correctamente.')
+                })
+                .catch((error) => {
+                    alert('Ocurrio un problema al enviar.')
+                })   
+        }
+    }, [validationForm]);
 
     return(
     <div className="formContainer">
@@ -74,7 +121,7 @@ const Form = () => {
                 </select>
                 <input onBlur={handleChangeName} className="name formItem" type="text" name="nombre" placeholder="Nombre" required/>
                 <input onBlur={handleChangeEmail} className="email formItem" type="email" name="email" placeholder="Ingresa un E-mail" required/>
-                <input onClick={handleChangeClick} className="enviar formItem" type="button" value="Enviar"/>
+                <input onClick={handleChangeSubmit} className="enviar formItem" type="button" value="Enviar"/>
             </form>
         </div>
     </div>
